b0b50e22d21178cafd3bf59f100b707623db95c8
nattygee <nattygee@live.com>
2025-05-07 07:21:27 -0400

    push to main remote ffs

--- src/App.js
+++ src/App.js
@@ -70,8 +70,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
-    [2, 4, 3]
+    [2, 4, 6]
   ];
 
   for (let i = 0; i < lines.length; i++) {
